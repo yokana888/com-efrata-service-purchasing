9a094594d47a6d76ac4a09e43af618e03f9b8c06
MatthewADIAN <hartonomatthew0@gmail.com>
2023-04-19 14:20:50 +0700

    Fix Pdf Template

--- Com.Efrata.Service.Purchasing.Lib/PDFTemplates/GarmentUnitExpenditureNotePDFTemplate.cs
+++ Com.Efrata.Service.Purchasing.Lib/PDFTemplates/GarmentUnitExpenditureNotePDFTemplate.cs
@@ -41,7 +41,7 @@ namespace Com.Efrata.Service.Purchasing.Lib.PDFTemplates
             Paragraph title = new Paragraph(titleString, header_font) { Alignment = Element.ALIGN_CENTER };
             document.Add(title);
 
-            string companyNameString = "PT EFRATA RETAILINDO";
+            string companyNameString = "PT EFRATA GARMINDO UTAMA";
             Paragraph companyName = new Paragraph(companyNameString, bold_font) { Alignment = Element.ALIGN_LEFT };
             document.Add(companyName);
 
