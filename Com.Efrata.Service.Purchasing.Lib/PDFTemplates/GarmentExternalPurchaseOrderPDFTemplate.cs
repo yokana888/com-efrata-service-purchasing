63129c1b7b4599e5ad4cc02a85a5db2af475b629
YOKANA <yokana.adita@outlook.com>
2023-04-26 11:27:44 +0700

    Fix Pdf Template

--- Com.Efrata.Service.Purchasing.Lib/PDFTemplates/GarmentExternalPurchaseOrderPDFTemplate.cs
+++ Com.Efrata.Service.Purchasing.Lib/PDFTemplates/GarmentExternalPurchaseOrderPDFTemplate.cs
@@ -46,7 +46,7 @@ namespace Com.Efrata.Service.Purchasing.Lib.PDFTemplates
             PdfPCell cellHeaderContentRight = new PdfPCell() { Border = Rectangle.NO_BORDER, HorizontalAlignment = Element.ALIGN_RIGHT };
 
             //cellHeaderContentLeft.Phrase = new Phrase("PT DAN LIRIS" + "\n" + "JL. Merapi No.23" + "\n" + "Banaran, Grogol, Kab. Sukoharjo" + "\n" + "Jawa Tengah 57552 - INDONESIA" + "\n" + "PO.BOX 166 Solo 57100" + "\n" + "Telp. (0271) 740888, 714400" + "\n" + "Fax. (0271) 735222, 740777", bold_font);
-            cellHeaderContentLeft.Phrase = new Phrase("PT EFRATA RETAILINDO" +  "\n" + "Banaran, Grogol, Sukoharjo" + "\n" + "Jawa Tengah 57552 - INDONESIA" + "\n" + "Telp (+62 271)719911, (+62 21)2900977", bold_font);
+            cellHeaderContentLeft.Phrase = new Phrase("PT EFRATA GARMINDO UTAMA" +  "\n" + "Banaran, Grogol, Sukoharjo" + "\n" + "Jawa Tengah 57552 - INDONESIA" + "\n" + "Telp (+62 271)719911, (+62 21)2900977", bold_font);
             tableHeader.AddCell(cellHeaderContentLeft);
 
             string noPO = EPONo;
@@ -131,7 +131,7 @@ namespace Com.Efrata.Service.Purchasing.Lib.PDFTemplates
 
             if (viewModel.Supplier.Import)
             {
-                string p1 = "The undersigned below, PT. EFRATA RETAILINDO, SOLO (hereinafter referred to as parties Purchasers) and " + viewModel.Supplier.Name + " (hereinafter referred to as seller\'s side) mutually agreed to enter into a sale and purchase contract with the following conditions: ";
+                string p1 = "The undersigned below, PT. EFRATA GARMINDO UTAMA, SOLO (hereinafter referred to as parties Purchasers) and " + viewModel.Supplier.Name + " (hereinafter referred to as seller\'s side) mutually agreed to enter into a sale and purchase contract with the following conditions: ";
                 Paragraph firstParagraph = new Paragraph(p1, normal_font) { Alignment = Element.ALIGN_LEFT };
 
                 firstParagraph.SpacingAfter = 10f;
@@ -139,7 +139,7 @@ namespace Com.Efrata.Service.Purchasing.Lib.PDFTemplates
             }
             else
             {
-                string p1 = "Dengan hormat,\nYang bertanda tangan di bawah ini, PT. EFRATA RETAILINDO, SOLO (selanjutnya disebut sebagai pihak Pembeli) dan " + viewModel.Supplier.Name + " (selanjutnya disebut sebagai pihak Penjual) saling menyetujui untuk mengadakan kontrak jual beli dengan ketentuan sebagai berikut: ";
+                string p1 = "Dengan hormat,\nYang bertanda tangan di bawah ini, PT. EFRATA GARMINDO UTAMA, SOLO (selanjutnya disebut sebagai pihak Pembeli) dan " + viewModel.Supplier.Name + " (selanjutnya disebut sebagai pihak Penjual) saling menyetujui untuk mengadakan kontrak jual beli dengan ketentuan sebagai berikut: ";
                 Paragraph firstParagraph = new Paragraph(p1, normal_font) { Alignment = Element.ALIGN_LEFT };
 
                 firstParagraph.SpacingAfter = 10f;
