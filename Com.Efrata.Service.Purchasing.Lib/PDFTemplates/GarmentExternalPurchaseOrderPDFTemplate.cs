fa6f5d607ae696651f1a99333621e55658df33d1
achensetyawati <achenzetya@gmail.com>
2023-12-14 11:45:23 +0700

    Update GarmentExternalPurchaseOrderPDFTemplate.cs

--- Com.Efrata.Service.Purchasing.Lib/PDFTemplates/GarmentExternalPurchaseOrderPDFTemplate.cs
+++ Com.Efrata.Service.Purchasing.Lib/PDFTemplates/GarmentExternalPurchaseOrderPDFTemplate.cs
@@ -620,7 +620,7 @@ namespace Com.Efrata.Service.Purchasing.Lib.PDFTemplates
             {
                 cellSignatureContent.Phrase = new Phrase("Pembeli\n\n\n\n\n\n\n(  " + viewModel.CreatedBy + "  )", bold_font);
                 tableSignature.AddCell(cellSignatureContent);
-                cellSignatureContent.Phrase = new Phrase("Manager\n\n\n\n\n\n\n(                         )", bold_font);
+                cellSignatureContent.Phrase = new Phrase("Manager\n\n\n\n\n\n\n( Bambang / Reski Taufik )", bold_font);
                 tableSignature.AddCell(cellSignatureContent);
             }
 
