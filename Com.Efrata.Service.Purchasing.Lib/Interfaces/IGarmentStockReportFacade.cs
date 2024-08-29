add9aa80ccdb27ce82fba0734504685aece85b32
ardana96 <ardanagama@gmail.com>
2024-08-29 13:30:02 +0700

    Add API Stock Per Product - SPRINT 18 - 01

--- Com.Efrata.Service.Purchasing.Lib/Interfaces/IGarmentStockReportFacade.cs
+++ Com.Efrata.Service.Purchasing.Lib/Interfaces/IGarmentStockReportFacade.cs
@@ -10,5 +10,6 @@ namespace Com.Efrata.Service.Purchasing.Lib.Interfaces
     {
         Tuple<List<GarmentStockReportViewModel>, int> GetStockReport(int offset, string unitcode, string planPo, string tipebarang, int page, int size, string Order, DateTime? dateFrom, DateTime? dateTo);
         MemoryStream GenerateExcelStockReport(string ctg, string categoryname, string unitname, string unitcode, string planPo, DateTime? datefrom, DateTime? dateto, int offset);
+        Tuple<List<GarmentStockByProductReportViewModel>, int> GetStockByProduct(int offset, string productCode, int page, int size, string Order);
     }
 }
